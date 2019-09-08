5f39bff1cbc825e7fe204b6cd67396c44b737356
baxter-t <baxter.t98@gmail.com>
2019-09-08 14:52:34 +1000

    First commit

---
+++ organiserSite/js/main.js
@@ -0,0 +1,22 @@
+var request = new  XMLHttpRequest();
+
+request.open('GET', 'https://khpgnkcm69.execute-api.us-east-1.amazonaws.com/dev/todos', true);
+
+request.setRequestHeader("Content-Type", "application/json");
+
+request.onload = function() {
+	var data = JSON.parse(this.response);
+
+	if (request.status >= 200 && request.status < 400) {
+		data.forEach(todo => {
+			console.log(todo.text)
+
+			// Package and add to the todo section
+			$('#todoList').append('<p>' + '[' + todo.weight + '] ' + todo.text + '</p>');
+		})
+	} else {
+		console.log('error')
+	}
+};
+
+request.send();
